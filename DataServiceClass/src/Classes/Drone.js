0191a66334d930e97d646ef943bcbec429b9ab49
Goutham Peepala <goutham.peepala@citrix.com>
2018-04-15 20:39:32 +0530

    Init for DataServcies.

---
+++ DataServiceClass/src/Classes/Drone.js
@@ -0,0 +1,7 @@
+// Another Sub Class from Vehicle.
+// Can have more than one class extending a super class.
+import {Vehicle} from './Vehicle.js'
+
+export class Drone extends Vehicle{
+    
+}
---
+++ DataServiceClass/src/Services/fleet-data-service.js
@@ -0,0 +1,9 @@
+import {Car} from './Classes/Car.js'
+import {Drone} from './Classes/Drone.js'
+
+export class FleetDataService{
+    constructor(){
+        this.cars = [];
+        this.Drones = [];
+    }
+}
